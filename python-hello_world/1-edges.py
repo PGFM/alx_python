8f40f3c3b294e8b449869b1b5f5de0f2dd5c131b
PGFM <pregeefranklin@gmail.com>
2023-07-21 00:35:21 +0100

    py

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-first_3 = "Sch"
-last_2 = "ol"
-middle = "choo"
+first_3 = "Hol"
+last_2 = "on"
+middle = "olberto"
 print("First 3 letters: {}".format(first_3))
 print("Last 2 letters: {}".format(last_2))
 print("Middle word: {}".format(middle))
