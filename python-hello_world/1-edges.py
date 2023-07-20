d6f9b1982e0dbb2f0a57ab1135d10d101a01d0f0
PGFM <pregeefranklin@gmail.com>
2023-07-21 00:50:06 +0100

    applied slicing on the variables

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-first_3 = "Hol"
-last_2 = "on"
-middle = "olberto"
+first_3 = word[:3]
+last_2 = word[-2:]
+middle = word[1:-2]
 print("First 3 letters: {}".format(first_3))
 print("Last 2 letters: {}".format(last_2))
 print("Middle word: {}".format(middle))
