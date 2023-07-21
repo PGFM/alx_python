9d768a4676b519cdcca4250f45baf8a913011e9d
PGFM <pregeefranklin@gmail.com>
2023-07-21 01:28:23 +0100

    removed comma

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-print("Last digit of {}, is {}".format(number, last_digit))
+print("Last digit of {} is {}".format(number, last_digit))
 if last_digit > 5:
     print("greater than 5")
 elif last_digit == 0:
