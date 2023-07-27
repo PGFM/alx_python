3e77386075094006b2a724d12a8b26126f16c3ba
PGFM <pregeefranklin@gmail.com>
2023-07-27 13:53:37 +0100

    import decimal

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-def pow(a, b, precision=15):
-    result = 1
+# def pow(a, b, precision=15):
+#     result = 1
+#     if b < 0:
+#         a = 1 / a
+#         b = abs(b)
+#     for _ in range(b):
+#         result *= a
+#     return round(result, precision)
+from decimal import Decimal, getcontext
+
+def pow(a, b):
+    getcontext().prec = 28  # Set precision to a sufficient value
+    result = Decimal(1)
     if b < 0:
         a = 1 / a
         b = abs(b)
     for _ in range(b):
-        result *= a
-    return round(result, precision)
+        result *= Decimal(a)
+    return float(result)
