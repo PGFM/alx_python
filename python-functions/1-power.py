ecd55acd5fe9c20417bb2f9b67b1e40469c4dc07
PGFM <pregeefranklin@gmail.com>
2023-07-27 12:54:06 +0100

    floating point

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-def pow(a, b):
+# def pow(a, b):
+#     result = 1
+#     if b < 0:
+#         a = 1 / a
+#         b = abs(b)
+#     for _ in range(b):
+#         result *= a
+#     return result
+def pow(a, b, precision=10):
     result = 1
     if b < 0:
         a = 1 / a
         b = abs(b)
     for _ in range(b):
         result *= a
-    return result
+    return round(result, precision)
