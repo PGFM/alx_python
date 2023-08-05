47c6c608827b61b5a7554c1db7a4f30276b16293
PGFM <pregeefranklin@gmail.com>
2023-08-05 09:46:07 +0100

    a**b

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -6,7 +6,5 @@ def pow(a, b):
     result = 1
     for _ in range(b):
         result *= a
-    if result.is_integer():
-        return int(result)
-    else:
-        return float(format(result, '.22g'))  # format to 22 significant figures
+    return {a**b}
+    # return round(result, 10)  # round to 10 decimal places
