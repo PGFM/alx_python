e9e342dedc6173235f03716fb5f8f74c361eb2be
PGFM <pregeefranklin@gmail.com>
2023-09-07 22:29:55 +0100

    new changes

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b < 0:
-        a = 1 / a
-        b = -b
     result = 1
     for _ in range(b):
         result *= a
-    return (a**b)
+    return result
